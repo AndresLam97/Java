ccf62cae52ec7c96d1be1db3f599d51a36134160
Andres <lamankien1997@gmail.com>
2022-09-23 01:24:03 +0700

    Add a part of lesson six exercise (not done yet)

--- LearnAppium/src/LessonSix/LessonSixMainClass.java
+++ LearnAppium/src/LessonSix/LessonSixMainClass.java
@@ -2,22 +2,37 @@ package LessonSix;
 
 import javax.xml.stream.events.Characters;
 import java.sql.SQLOutput;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Locale;
-import java.util.Scanner;
+import java.util.*;
 
 public class LessonSixMainClass {
     public static void main(String[] args) {
-        LessonSixMainClass lessonSix = new LessonSixMainClass();
-        lessonSix.initMenu();
+        //LessonSixMainClass lessonSix = new LessonSixMainClass();
+        //lessonSix.initMenu();
+        String s = "asd hrs 50 min";
+        String[] list = s.split("[ ]");
+        int totalMinutes = 0;
+        for(int index = 0; index < list.length; index++)
+        {
+            switch(list[index].toLowerCase())
+            {
+                case "hrs":
+                case "hr":
+                case "hour":
+                case "hours":
+                    if(index == 0) {continue;}
+                    if(checkDigitString(list[index-1]))
+                    {
+                        totalMinutes += Integer.parseInt(list[index-1]);
+                    }
+                    System.out.println(Integer.valueOf(list[index - 1]));
+            }
+        }
+
     }
 
-    void initMenu()
-    {
+    void initMenu() {
         boolean checkValue = true;
-        while(checkValue)
-        {
+        while (checkValue) {
             System.out.println("===== LAB MENU =====");
             System.out.println("1. Lab 6.1.");
             System.out.println("2. Lab 6.2.");
@@ -27,8 +42,7 @@ public class LessonSixMainClass {
             System.out.print("Enter your option: ");
             int userOption = new Scanner(System.in).nextInt();
 
-            switch(userOption)
-            {
+            switch (userOption) {
                 case 1:
                     Lab61();
                     break;
@@ -51,85 +65,206 @@ public class LessonSixMainClass {
         }
     }
 
-    public void Lab61(){}
+    public void Lab61() {
+        // Declare variables
+        String userString;
+        String[] detachedList;
+        Map<Boolean,String> result = new HashMap<>();
 
-    public void Lab62(){
+        // Get user input
+        System.out.print("Enter your string: ");
+        userString = new Scanner(System.in).nextLine();
+
+        // Check string is empty or not
+        if(userString.isEmpty())
+        {
+            System.out.println("You entered an empty string. Please try again!");
+            return;
+        }
+
+        detachedList = userString.split("[ ]");
+        result = checkUserInputString(detachedList);
+
+        if(result.containsKey(false))
+        {
+            System.out.printf("%s", result.get(false));
+            return;
+        }
+
+
+    }
+
+    public void Lab62() {
+        // Declare variables
         String MY_PASSWORD = "password123";
         int totalEnterTime = 0;
+
+        // Start input and check password flow
         do {
             System.out.print("Enter your password: ");
             String passwordString = new Scanner(System.in).nextLine();
-            if(MY_PASSWORD.equals(passwordString))
-            {
+            if (MY_PASSWORD.equals(passwordString)) {
                 System.out.println("You entered the correct password. Congratulation!");
                 break;
-            }
-            else
-            {
-                if(totalEnterTime != 2){
-                System.out.println("You entered the wrong password. Please try again!");
-                totalEnterTime++;}
-                else{
+            } else {
+                if (totalEnterTime != 2) {
+                    System.out.println("You entered the wrong password. Please try again!");
+                    totalEnterTime++;
+                } else {
                     System.out.println("You entered incorrect password for 3 times. Please re-run the lab!");
                     totalEnterTime++;
                 }
             }
-        }while (totalEnterTime < 3);
+        } while (totalEnterTime < 3);
     }
 
-    public void Lab63(){
-        System.out.print("Enter your string: ");
-        String userString = new Scanner(System.in).nextLine();
+    public void Lab63() {
+        // Declare variables
+        String userString;
         String newString = new String("");
-        for(char character : userString.toCharArray())
-        {
-            if(Character.isDigit(character))
-            {
+
+        // Get user input
+        System.out.print("Enter your string: ");
+        userString = new Scanner(System.in).nextLine();
+
+        // Check string are empty or not
+        if (userString.equals("")) {
+            System.out.println("There is nothing to print because you was entered an empty string!");
+            return;
+        }
+
+        // Detach digits character in string
+        for (char character : userString.toCharArray()) {
+            if (Character.isDigit(character)) {
                 newString = newString.concat(String.valueOf(character));
             }
         }
-        if(userString.equals(""))
-        {
-            System.out.println("There is nothing to print because you was entered an empty string!");
-        }
-        else if(newString.equals(""))
-        {
+
+        // Display the result string with detached data
+        if (newString.equals("")) {
             System.out.println("There is no any digit in your string!");
-        }
-        else
-        {
+        } else {
             System.out.println("After detach: " + newString);
         }
     }
 
-    public void Lab64()
-    {
+    public void Lab64() {
+        // Declare variables
+        String myUrl;
+        Map<Boolean, String> result = new HashMap<>();
+        String[] list;
+
+        // Get user input
         System.out.print("Enter your url: ");
-        String myUrl = new Scanner(System.in).nextLine();
-        String[] list = myUrl.split("://|/");
-        for(int index = 0 ; index < list.length; index++)
-        {
-            System.out.println(list[index]);
+        myUrl = new Scanner(System.in).nextLine();
+
+        // Check string is empty or not
+        if (myUrl.isEmpty()) {
+            System.out.println("You entered an empty url. Please try again!");
+            return;
         }
+
+        // Split url to small parts
+        list = myUrl.split("://|/");
+
+        // Check the url data
+        result = checkURL(list);
+        if (result.containsKey(false)) {
+            System.out.println(result.get(false));
+            return;
+        }
+
+        // Display url information
+        displayUrlData(list);
+
     }
 
-    boolean checkUrl(String[] url)
+    public Map<Boolean,String> checkUserInputString(String[] list)
     {
-        boolean protocolKey = false;
-        boolean domainExtensionKey = false;
-        String
-        switch(url[0].toLowerCase())
+        int totalMinutes;
+        for(int index = 0; index < list.length; index++)
         {
-            case "http": protocolKey = true;
-            break;
-            case "https": protocolKey = true;
-            break;
-            default: protocolKey = false;
+            switch(list[index].toLowerCase())
+            {
+                case "hrs":
+                case "hr":
+                case "hour":
+                case "hours":
+                    if(index == 0) { continue;}
+            }
         }
 
+        return null;
     }
 
+    public static boolean checkDigitString(String string)
+    {
+        boolean result = true;
+        for(int index = 0 ; index < string.length() ; index++)
+        {
+            if(Character.isDigit(string.charAt(index))) { continue;}
+            return false;
+        }
+        return true;
+    }
 
+    public String getDomainSuffix(String domain) {
+        // Split domain to small parts
+        String[] detachList = domain.split("[.]");
+
+        // If domain name less than 2
+        // Means cannot detach the suffix
+        if (detachList.length < 2) {
+            return "Error";
+        }
+
+        return detachList[detachList.length - 1];
+    }
+
+    public Map<Boolean, String> checkURL(String[] list) {
+        Map<Boolean, String> result = new HashMap<>();
+        // Check protocol
+        switch (list[0].toLowerCase()) {
+            case "http":
+            case "https":
+                break;
+            default:
+                result.put(false, "You have entered an url with incorrect protocol, please retry again!");
+                return result;
+        }
+
+        // Check domain suffix
+        switch (getDomainSuffix(list[1]).toLowerCase()) {
+            case "com":
+                break;
+            case "net":
+            default:
+                result.put(false, "You have entered an url with incorrect domain's suffix, please retry again!");
+                return result;
+        }
+
+        result.put(true, "Correct URL");
+        return result;
+    }
+
+    public void displayUrlData(String[] list) {
+        // If url doesn't have suffix, display the information in the if statement
+        // If url have suffix, display the information in the else statement
+        if (list.length < 3) {
+            System.out.printf("Protocol: %s\n", list[0]);
+            System.out.printf("Domain: %s\n", list[1]);
+            System.out.printf("Domain suffix: %s\n", getDomainSuffix(list[1]));
+        } else {
+            System.out.printf("Protocol: %s\n", list[0]);
+            System.out.printf("Domain: %s\n", list[1]);
+            System.out.printf("Domain suffix: %s\n", getDomainSuffix(list[1]));
+            System.out.printf("Domain sub path: ");
+            for (int index = 2; index < list.length; index++) {
+                System.out.printf("/%s", list[index]);
+            }
+            System.out.printf("\n");
+        }
+    }
 
 
 }
